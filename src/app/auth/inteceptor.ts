afe7258fe25be2c8023fb27267f427fbb140028b
Amit Kumar Singh <amit.singh@tavant.com>
2018-10-29 19:03:07 +0530

    modified interceport service for error and redirection; To correct GUI redirection

--- src/app/auth/inteceptor.ts
+++ src/app/auth/inteceptor.ts
@@ -6,6 +6,8 @@ import { Router } from '@angular/router';
 import {TokenStorage} from './token.storage';
 //import { tap } from 'rxjs/operators';
 import {tap} from 'rxjs/internal/operators';
+import { catchError } from 'rxjs/operators';
+import { of } from 'rxjs/observable/of';
  
 const TOKEN_HEADER_KEY = 'Authorization';
 
@@ -20,20 +22,26 @@ export class Interceptor implements HttpInterceptor {
     if (this.token.getToken() != null) {
       authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, 'Bearer ' + this.token.getToken())});
     }
-    return next.handle(authReq).pipe(tap(
-        (err: any) => {
-          if (err instanceof HttpErrorResponse) {
-            console.log(err);
-            console.log('req url :: ' + req.url);
-            if (err.status === 401) {
-              this.router.navigateByUrl('');
-            }
-            if (err.status === 403) {
-              this.router.navigate(['']);
-            }
-          }
-        }
-      ));
+    
+    return next.handle(authReq).pipe(catchError((error, caught) => {
+      //intercept the respons error and displace it to the console
+      console.log(error);
+      this.handleAuthError(error);
+      return of(error);
+    }) as any);
+
   }
+
+  private handleAuthError(err: HttpErrorResponse): Observable<any> {
+      //handle your auth error or rethrow
+      if (err.status === 401 || err.status === 403) {
+        //navigate /delete cookies or whatever
+        console.log('handled error ' + err.status);
+        this.router.navigate(['']);
+        // if you've caught / handled the error, you don't want to rethrow it unless you also want downstream consumers to have to handle it as well.
+        return of(err.message);
+      }
+      throw err;
+    }
 
 }
